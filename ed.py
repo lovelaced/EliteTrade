4a9f71866cd219ac47534bf369fabbaad7156ef8
Anton Vilhelm Ásgeirsson <anton@antonvilhelm.is>
2014-12-23 00:56:33 +0000

    cleanup

--- ed.py
+++ ed.py
@@ -30,7 +30,7 @@ class systems:
             self.list.append(system(n[0], n[1], n[2], n[3]))
     
     def add(self, system_name, x, y, z):
-        global system
+        global system #needed because otherwise system falls into local namespace.
         new_system = system(system_name, x, y, z)
         cur.execute("INSERT INTO systems (name, xcoord, ycoord, zcoord) VALUES (%s, %s, %s, %s)",
                 (new_system.name, new_system.x, new_system.y, new_system.z))
@@ -53,43 +53,9 @@ class systems:
         conn.commit()
 
 
-
-# distance from start in ly
-fromstart = 0
-i = 0
-# number of stops on trip
-stops = 0
- 
-#while i < len(syslist):
-#    currx = syslist[i].x
-#    curry = syslist[i].y
-#    currz = syslist[i].z
-#    currsys = syslist[i].system_name
-#    # possible jumps from this system based on proximity
-#    possiblejumps = []
-#    j = 0
-#    while j < len(syslist):
-#        if 0 < lightyears < 60:
-#            possiblejumps.append(j)
-#            # something like this maybe I dunno
-#            # fromstart += lightyears
-#            # print currsys, ocurrsys
-#            # stops += 1
-#            # if fromstart > 150 and stops is 4 or stops is 5:
-#            # print currsys, ocurrsys
-#            # print "done!"
-#            # elif stops > 6:
-#            # i-=2
-#            # break
-#            j += 1
-#            i += 1
-#     
-# gets distance from any system to any system
-#print(syslist[0].distancefrom[3]) 
 if __name__ == '__main__':
     sys = systems()
     sys.reset_distances()
     #Examples
     #sys.add("39 Tauri", -7.31, -20.28, -50.91)
     #sys.add("Aegaenon", 46.91, 23.63, -59.75)
-    print(cur.execute("SELECT * FROM distances"))
